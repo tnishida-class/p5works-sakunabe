e0933c18d8c23f2fda02d4b8f183d31a8ebe0d49
sakunabe <72489954+sakunabe@users.noreply.github.com>
2020-10-20 11:44:46 +0900

    practice2-2

--- section2-2/sketch.js
+++ section2-2/sketch.js
@@ -9,10 +9,13 @@ function setup() {
   background(196);
   for(let i = 0; i < 9; i++){
     // 変えるのはここから
-    if(i % 2 == 0){
+    if(i %3==0){
       strokeWeight(2);
     }
-    else{
+    else if(i%3==1){
+      strokeWeight(3);
+    }
+    else {
       strokeWeight(1);
     }
     // ここまでの間になります
