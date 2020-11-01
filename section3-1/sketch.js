d0a1e37fae162d2736f512f8921fc4c80cdfa379
sakunabe <72489954+sakunabe@users.noreply.github.com>
2020-11-01 17:56:32 +0900

    3-1

--- section3-1/sketch.js
+++ section3-1/sketch.js
@@ -2,11 +2,24 @@
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
   createCanvas(200, 200);
-  background(192);
+  background(0,0,255);
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
     let x = 100 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+
+fill(255,255,0);
+star(x,y,10);
+    function star(cx, cy, r){
+      beginShape();
+      for(var i = 0; i < 5; i++){
+        let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+        let x = cx + cos(theta) * r;
+        let y = cy + sin(theta) * r;
+        vertex(x,y);
+      }
+      endShape(CLOSE);
+    }
+
   }
 }
