13fd203c2439457661bb9afb3d79f2dd85866a81
sakunabe <72489954+sakunabe@users.noreply.github.com>
2020-11-16 23:58:46 +0900

    section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -9,10 +9,20 @@ function setup(){
   vy = 8;
 }
 
+function mouseClicked(){
+  x=mouseX;
+  y=mouseY;
+}
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
-
+push();
+x += vx/2;
+y += vy/2;
+translate(x,y);
+rotate(frameCount  /50);
+star(0,0,30,70,5);
+pop();
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
@@ -24,7 +34,7 @@ function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
     var theta = TWO_PI * i * 2 / 5 - HALF_PI;
-    // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+  // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
